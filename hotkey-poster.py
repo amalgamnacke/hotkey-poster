a7cc6e0147faeba17501e8d378039c76748ee68f
amalgamnacke <gustav.lattman@gmail.com>
2019-08-25 21:58:41 +0200

    Added option to add auth headers to requests.

--- hotkey-poster.py
+++ hotkey-poster.py
@@ -1,6 +1,9 @@
 import keyboard
 import configparser
 from urllib import request, parse
+
+import urllib.request
+import urllib.response
         
 class HotkeyPoster:
     """HotkeyPoster."""
@@ -10,7 +13,7 @@ class HotkeyPoster:
     
     def __init__(self):
         # Read config file.
-        configParser = configparser.ConfigParser()
+        configParser = configparser.ConfigParser(delimiters=("="))
         configParser.read("config.ini")
 
         self.config = dict(configParser.items("Config"))
@@ -20,6 +23,7 @@ class HotkeyPoster:
 
     def registerHotkeys(self):
         for hotkey, soundFileId in self.hotkeys.items():
+            print("Registering hotkey '{}' for sound '{}'.".format(hotkey, soundFileId))
             keyboard.add_hotkey(hotkey, self.playSoundById, args=(self.config["username"], soundFileId))
 
     def start(self):
@@ -35,10 +39,25 @@ class HotkeyPoster:
         # Prepare URL.
         url = self.config["baseurl"] + self.config["uriplaysoundbyid"]
 
-        # Perform POST request.
-        req = request.Request(url, data = payload)
+        # Prepare auth.
+        authUsername = self.config["authusername"]
+        authPassword = self.config["authpassword"]
         
-        resp = request.urlopen(req)
+        # Perform POST request.
+        if self.config["addauthheaders"] == "true":
+            passwordManager = urllib.request.HTTPPasswordMgrWithDefaultRealm()
+            passwordManager.add_password(None, url, authUsername, authPassword)
+            
+            authHandler = urllib.request.HTTPBasicAuthHandler(passwordManager)
+
+            opener = urllib.request.build_opener(authHandler)
+            urllib.request.install_opener(opener)
+            
+            req = urllib.request.Request(url, data = payload)
+            resp = opener.open(req)
+        else:
+            req = request.Request(url, data = payload)
+            resp = request.urlopen(req)
 
 a = HotkeyPoster()
 a.start()
